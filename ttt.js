0b66d51375b6f718e78735c3f81ee8a8b7fe0ab9
ian <20951604+iansecretario@users.noreply.github.com>
2024-01-19 06:23:24 +0800

    Create ttt.js

---
+++ ttt.js
@@ -0,0 +1,7 @@
+document.getElementById('triggerModalButton').addEventListener('click', function() {
+    document.getElementById('myModal').style.display = 'block';
+});
+
+document.getElementById('closeModal').addEventListener('click', function() {
+    document.getElementById('myModal').style.display = 'none';
+});
