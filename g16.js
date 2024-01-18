71e794b582f6b16031dec03ae2e3e61b041201ba
ian <20951604+iansecretario@users.noreply.github.com>
2024-01-18 13:36:42 +0800

    Update g16.js

--- g16.js
+++ g16.js
@@ -1,28 +1,23 @@
-let eventListenerAdded = false; // Variable to track if the event listener has been added
-
 function addEventToButtons(answer) {
-    if (!eventListenerAdded) { // Check if the event listener has been added
-        const buttons = document.querySelectorAll('.guidem-button');
-        buttons.forEach(function (button) {
-            button.addEventListener('click', function () {
-                const formId = button.id.replace("submit-button", "gflag-form");
-                const inputId = button.id.replace("submit-button", "gflag-input");
-                const form = document.getElementById(formId);
-                const input = document.getElementById(inputId);
-                if (input) {
-                    const userInput = input.value;
-                    const userInputHash = sha256(userInput);
-                    if (userInputHash === answer) {
-                        displayAlert('Correct Answer!', 'green');
-                    } else {
-                        displayAlert('Incorrect Answer! Please try again.', 'red');
-                    }
+    const buttons = document.querySelectorAll('.guidem-button');
+    buttons.forEach(function (button) {
+        button.addEventListener('click', function () {
+            const formId = button.id.replace("submit-button", "gflag-form");
+            const inputId = button.id.replace("submit-button", "gflag-input");
+            const form = document.getElementById(formId);
+            const input = document.getElementById(inputId);
+            if (input) {
+                const userInput = input.value;
+                const userInputHash = sha256(userInput);
+                if (userInputHash === answer) {
+                    displayAlert('Correct Answer!', 'green');
+                } else {
+                    displayAlert('Incorrect Answer! Please try again.', 'red');
                 }
-            });
-            console.log("Event added to button:", button.id);
+            }
         });
-        eventListenerAdded = true; // Set the flag to indicate that the event listener has been added
-    }
+        console.log("Event added to button:", button.id);
+    });
 }
 
 // Start the process for buttons with class 'guidem-button' using the answer hash
@@ -32,7 +27,7 @@ function sha256(input) {
     // Implement SHA-256 hashing here (you can use a library or write your own)
     // Return the hash value as a string
     // For testing purposes, you can use a simple function like this:
-    // return input; // Replace this with actual SHA-256 hashing
+    return input; // Replace this with actual SHA-256 hashing
 }
 
 function displayAlert(message, color) {
