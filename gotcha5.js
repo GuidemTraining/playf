e3ea64911ce9c889ea413e9fa9bbe65d64d4849f
ian <20951604+iansecretario@users.noreply.github.com>
2024-01-20 07:09:18 +0800

    Update gotcha5.js

--- gotcha5.js
+++ gotcha5.js
@@ -31,7 +31,6 @@ $(document).ready(function() {
               ${hint}
             </div>
             <div class="modal-footer">
-              <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
             </div>
           </div>
         </div>
